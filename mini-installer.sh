3a687301fc5d5051d5ff164d4843a3f425515e7d
Russel Monteiro <russelmonteiro93@gmail.com>
2020-10-07 11:17:54 +0530

    Create mini-installer.sh

---
+++ mini-installer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo -e "deb https://deb.parrot.sh/parrot/ rolling main contrib non-free
+        #deb-src https://deb.parrot.sh/parrot/ rolling main contrib non-free
+        deb https://deb.parrot.sh/parrot/ rolling-security main contrib non-free
+        #deb-src https://deb.parrot.sh/parrot/ rolling-security main contrib non-free" > /etc/apt/sources.list.d/parrot.list
+
+apt-key adv --keyserver keys.gnupg.net --recv-keys B56FFA946EB1660A
+apt update
+apt install parrot-pico
